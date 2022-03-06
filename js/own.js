46951264768e6b1447d149b18a0dd700a772c830
George Gusakov <g-gusakov@list.ru>
2022-03-06 23:31:37 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	index.html

---
+++ js/own.js
@@ -0,0 +1,11 @@
+let Hello1 = document.getElementById('nav_hello1')
+let Hello2 = document.getElementById('nav_hello2')
+let navb = document.getElementById('navb')
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    Hello1.style.right = value*0.5 + 'px';
+    Hello2.style.left = value*0.5 + 'px';
+    navb.style.left = value*2 + 'px';
+
+})
